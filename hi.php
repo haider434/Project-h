0acbf5058b7f9a2172b55d68d15511d851d8b45b
haider434 <122698445+haider434@users.noreply.github.com>
2023-01-15 01:22:57 +0500

    Add files via upload

---
+++ hi.php
@@ -0,0 +1,274 @@
+<?php
+if(isset($_POST['contact_btn'])){
+    $name=$_POST['user_name'];
+    $email=$_POST['user_email'];
+    $password=$_POST['user_password'];
+    $msg=$_POST['user_msg'];
+    $to= "malikhaiderali434@gmail.com";
+    $header=from"user_email"
+    
+
+}
+
+
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Canvas</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Poppins&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/429bc2444b.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Work+Sans&display=swap" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <section class="colored-section">
+        <div class="container-fluid container-lg-12  container-md-12 container-sm-12 ">
+            <nav class=" navbar navbar-expand-lg navbar s" style="background-color: white;">
+                <img class="navbar-brand" src="images/logo.png" alt="">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+           <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+           <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+               <li class="nav-item">
+                   <a class="nav-link" href="">Home</a>
+               </li>
+               <li class="nav-item">
+                   <a class="nav-link" href="">Features</a>
+               </li>
+              <li class="nav-item">
+                   <a class="nav-link" href="">Pages</a>
+               </li>
+               <li class="nav-item">
+                   <a class="nav-link" href="">Portfolio</a>
+               </li>
+               <li class="nav-item">
+                   <a class="nav-link" href="">Blog</a>
+               </li>
+               <li class="nav-item">
+                   <a class="nav-link" href="">Shop</a>
+               </li>
+               <li class="nav-item">
+                <a class="nav-link" href="">Shortcodes</a>
+            </li>
+               
+           </ul>
+           
+            <div class="column b">
+                <i class="fa-sharp fa-solid fa-magnifying-glass t"></i>
+                <i class="fa-thin fa-solid fa-lock t"></i>
+            </div>
+        </div>  
+      </nav>
+    </section>
+    <hr>
+   <br>
+   <br>
+   <br>
+   <br>
+   <br>
+   <br>
+    <section>
+<div class=" row" >
+    <div class="col-lg-6 col-sm-12 column1" style="float: left; clear: left; align-items: center;">
+        <img src="images/niche.jpg" alt="">
+        <div class="text">
+        <h3>
+            Niche Demos </h3>
+        <p>55+ Structured <br> Premium Demos for Every <br>
+          <span class="i"> Browse Demos </span> </p>
+        </div>
+    </div>
+    <div class="col-lg-6 col-sm-12 columni "  >
+        <img class="colm" src="images/multipages.jpg" alt="">
+        <div class="tixt">
+        <h3>Multi-Page Layouts</h3>
+         <p>Classic Demos with a Lot <br> of Homepages Options. <br>
+           <span class="i"> Browse Multi-Pages</span> </p>
+        </div>
+    </div>
+    <div class="row2">
+    <div class="col-lg-6 col-sm-12  i3" style="float: left; clear: left; align-items: center;">
+        <img class="imgone" src="images/onepages.jpg" alt="">
+        <div class="text2">
+       <h3> One-Page Demos</h3>
+       <p> 65+ One-Page Demos <br> for a Quick & Effective <br> Landing Page. <br>
+      <span class="i">  Browse One-Pages </span></p>
+    </div>
+    </div>
+    <div class="col-lg-6 col-sm-12 columni2">
+        <img class="colmimg" src="images/rev-slider.jpg" alt="">
+        <div class="tixt2">
+       <h3>Hero Slider Templates
+        </h3>
+       <p> 140+ Premium Templates <br> to Kickstart your <br> Hero/Slider Section.
+        <br>
+      <a href="#" class="i">  Browse More </a></p>
+    </div>
+    </div>
+</div>
+</div>
+</section>
+<section class="grry">
+<div class=" row " style="position: relative;">
+    <div class="feature-div col-lg-3 th">
+        <h3 class="gry2">68000+</h3>
+         <p> Happy Customers</p>
+    </div>
+    <div class="feature-div col-lg-3 th">
+        <h3 class="gry2">1200+</h3>
+         <p> Awesome Example Templates</p>
+    </div>
+    <div class="feature-div col-lg-3 th">
+        <h3 class="gry2">55+</h3>
+         <p>Beautiful Niche Demos</p>
+    </div>
+    <div class="feature-div col-lg-3 th">
+        <h3 class="gry2">65+</h3>
+        <p>One page Demos</p>
+    </div>
+    <div class="feature-div col-lg-3 th" >
+        <h3 class="gry2">140+</h3>
+        <p>  Revolution Slider Layouts
+          Happy Customers</p>
+    </div>
+    <div class="feature-div col-lg-3 th">
+        <h3 class="gry2">1500+</h3>
+         <p>UI Features & Shortcodes</p>   
+       
+    </div>
+    <div class="feature-div col-lg-3 th">
+        <h3 class="gry2"><i class="fa-solid fa-infinity"></i></h3>
+         <p>Header Styles</p>
+    </div>
+    <div class="feature-div col-lg-3 th">
+        <h3 class="gry2">100%</h3>
+         <p>Reapnsive and Costomisable</p>
+    </div>
+</div>
+</section>
+<section class="themes">
+    <h1 class="h21"> <span class="h1">Niche</span>  Demos</h1>
+<div class="row">
+    <div class="feature-div col-lg-4">
+        <img src="images/classic.jpg" alt="">
+        <h3>Classic</h3>
+</div>
+<div class="feature-div col-lg-4">
+<img src="images/construction.jpg" alt="">
+<h3>Real estate</h3>
+</div>
+<div class="feature-div col-lg-4">
+    <img src="images/seo.jpg" alt="">
+    <h3>SEO</h3>
+</div>
+<div class="feature-div col-lg-4">
+    <img src="images/construction.jpg" alt="">
+    <h3>Construction</h3>
+</div>
+<div class="feature-div col-lg-4">
+    <img src="images/travel.jpg" alt="">
+    <h3>Travel</h3>
+</div>
+<div class="feature-div col-lg-4">
+    <img src="images/medical.jpg" alt="">
+    <h3>Medical</h3>
+</div>
+<div class="feature-div col-lg-4">
+    <img src="images/landing.jpg" alt="">
+    <h3>Landing</h3>
+</div>
+<div class="feature-div col-lg-4">
+    <img src="images/law-firm.jpg" alt="">
+    <h3>Law Firm</h3>
+</div>
+<div class="feature-div col-lg-4">
+    <img src="images/kindergarten.jpg" alt="">
+    <h3>Kindergarden</h3>
+</div>
+
+</div></section>
+<section class="loved">
+<div class="row4">
+    <h1 class="h21">Loved by <span class="h1"> 68000+</span> Happy Customers</h1>
+   <h4> Powered by Top-Notch Support & Stellar Customer Reviews </h4>
+
+</div>
+
+</section>
+<section class="rating">
+<div class="row">
+    <div class="feature-div col-lg-4 col-md-6 col-sm-12 m1">
+        <div class="m1txt">
+        <a href=""><i class="fa-regular fa-images o"></i></a>
+    <h1>165+</h1>
+      <h4>Portfolio Layouts</h4>
+    </div>
+    </div>
+    <div class="feature-div col-lg-4 col-md-6 col-sm-12 m2">
+        <div class="m1txt">
+        <a href=""><i class="fa-classic fa-pencil o"></i></a>
+    <h1>45+</h1>
+      <h4>  Blog Templates</h4>
+    </div>
+    </div>
+    <div class="feature-div col-md-6 col-sm-12 col-lg-4 m3">
+        <div class="m1txt">
+        <a href=""><i class="fa-sharp fa-solid fa-puzzle-piece o"></i></a>
+    <h1>50+</h1>
+      <h4>Short Codes</h4>
+    </div>
+    </div>
+    <div class="feature-div col-md-6 col-sm-12 col-lg-4 m4">
+        <div class="m1txt">
+        <a href=""><i class="fa-regular fa-images o"></i></a>
+    <h1>100+</h1>
+      <h4>PSD Files</h4>
+    </div>
+    </div>
+</div>
+</section>
+<section class="tmplt">
+<div>
+<h1 class="template"><span class="tmpl1"> 1200+</span> Templates to minimize your Development Time!</h1>
+</div>
+</section>
+<form action="" >
+    <div class="_post">
+        <h1>Contact Us</h1>
+        <br>
+        <h3>Name</h3>
+        <input type="*text" placeholder="Enter Name..." id="user_name"><br>
+        <h3>Email</h3>
+<input type="email" name="user_email" id="email" placeholder="Enter your Email here..."><br>
+<h3>Password</h3>
+<input type="password" name="user_password" id="password" placeholder="Password..."><br>
+<h3>Message</h3>
+<textarea name="user_msg" id="textarea" cols="4" rows="2"></textarea>
+
+<br>
+<br>
+
+<a href=""><input class="btn btn-primary btn-outline-dark" type="button "name="contact_btn" value="Send"></a>
+    </div>
+</form>
+    <footer style="background-color: rgba(0, 0, 0, 0.877); color: rgba(255,255,255,.4); margin-top: 10rem; padding-top: 1.5%; padding-bottom: 1%;">
+<p>Copyrights © 2020 All Rights Reserved by Haider</p>
+    </footer>
+  
+</body>
+</html>
